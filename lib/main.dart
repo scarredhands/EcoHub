f52417f43c6aa48e62ba273e4e420f7d679fbfe8
Yatika Jena <j.yatika@iitg.ac.in>
2024-06-25 02:48:26 +0530

    made the disease detection screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:ecohub/screens/authentication_screens/login_screen.dart';
 import 'package:ecohub/screens/home_screen.dart';
+import 'package:ecohub/services/auth_wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: AuthWrapper(),
       routes: {
         '/home': (context) => HomeScreen(),
       },
